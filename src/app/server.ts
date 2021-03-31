0a66de6d596ec1b9bdb7859bc99341037cb6c162
Ulf Winkelvos <code@ulf.winkelvos.de>
2021-04-01 01:36:35 +0200

    use router middleware instead of route

--- src/app/server.ts
+++ src/app/server.ts
@@ -1,44 +1,34 @@
 import Koa from "koa";
-import Body from "koa-body";
-import Route from "koa-route";
+import bodyParser from "koa-bodyparser";
+import Router from "@koa/router";
 import * as z from "zod";
 
 import { NameSchema } from "../lib/index";
 import { routeHandler } from "../lib/middleware";
 
 const app = new Koa();
-app.use(Body());
+const router = new Router({
+	prefix: '/users'
+});
+router.get("/:id", routeHandler(
+	{
+		params: z.object({ id: z.string().regex(/^\d+$/, "must be a number").transform(id => Number(id)) }),
+	},
+	async ({ params }) => {
+		return { resBody: { params } };
+	}
+));
+router.post("/", bodyParser(), routeHandler(
+	{ body: NameSchema, query: z.object({ debug: z.string().optional() }) },
+	async ({ body, query }) => {
+		if (query.debug) {
+			return { resBody: `Hello ${body.first} ${body.middle ? body.middle + " " : ""}${body.last}!\n` };
+		} else {
+			return { resStatus: 204 };
+		}
+	}
+));
 
-app.use(
-	Route.get("/user/:id")(
-		routeHandler(
-			{
-				params: z
-					.string()
-					.array()
-					.length(1)
-					.transform(([id]) => ({ id: Number(id) })),
-			},
-			async ({ params }) => {
-				return { resBody: { params } };
-			}
-		)
-	)
-);
-
-app.use(
-	Route.post("/user")(
-		routeHandler(
-			{ body: NameSchema, query: z.object({ debug: z.string().optional() }), params: z.string().array().length(0) },
-			async ({ body, query }) => {
-				if (query.debug) {
-					return { resBody: `Hello ${body.first} ${body.middle ? body.middle + " " : ""}${body.last}!\n` };
-				} else {
-					return { resStatus: 204 };
-				}
-			}
-		)
-	)
-);
+app.use(router.routes());
 
 app.listen(3000);
--- src/lib/middleware.ts
+++ src/lib/middleware.ts
@@ -1,11 +1,12 @@
 import * as z from "zod";
 import * as Koa from "koa";
+import { Middleware } from "@koa/router";
 import { ParsedUrlQuery } from "querystring";
 
 interface RequestSchemas<B = never, Q = never, P = never> {
 	body?: z.ZodType<B, z.ZodAnyDef, unknown>;
 	query?: z.ZodType<Q, z.ZodAnyDef, ParsedUrlQuery>;
-	params?: z.ZodType<P, z.ZodAnyDef, string[]>;
+	params?: z.ZodType<P, z.ZodAnyDef, unknown>;
 }
 
 interface RequestData<B = never, Q = never, P = never> {
@@ -14,7 +15,6 @@ interface RequestData<B = never, Q = never, P = never> {
 	params: P;
 }
 
-type RouteHandler = (this: Koa.Context, ctx: Koa.Context, ...params: any[]) => any;
 type HandlerResult =
 	| { resBody: object | string | Buffer; resStatus?: number }
 	| { resBody?: object | string | Buffer; resStatus: number };
@@ -22,14 +22,14 @@ type HandlerResult =
 function routeHandler<B = never, Q = never, P = never>(
 	schemas: RequestSchemas<B, Q, P>,
 	handler: (data: RequestData<B, Q, P>) => Promise<HandlerResult>
-): RouteHandler {
-	return async function (this: Koa.Context, ctx: Koa.Context, ...rawParams: string[]) {
+): Middleware {
+	return async function (ctx: Koa.Context) {
 		let data: RequestData<B, Q, P>;
 		try {
 			data = {
 				body: schemas.body?.parse(ctx.request.body)!, // TODO: can these assertions ! be replaced by proper typing? generic defaults?
 				query: schemas.query?.parse(ctx.query)!, // TODO: can these assertions ! be replaced by proper typing? generic defaults?
-				params: schemas.params?.parse(rawParams.slice(0, -1))!, // TODO: can these assertions ! be replaced by proper typing? generic defaults?
+				params: schemas.params?.parse(ctx.params)!, // TODO: can these assertions ! be replaced by proper typing? generic defaults?
 			};
 		} catch (err) {
 			ctx.throw(400, err);
